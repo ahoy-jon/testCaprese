27eb239597907f785a3a674c0d0d6c6431e1e660
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-05-09 17:54:16 +0200

    add implementation to Context as Option

--- captureAwareFuncs.scala
+++ captureAwareFuncs.scala
@@ -1,25 +1,33 @@
 package captureAware
 
-class Context extends caps.Capability
+import scala.annotation.publicInBinary
+import scala.util.control.{ControlThrowable, NoStackTrace}
+
 
 object WithContext:
   def pushRight[A, B](f: Context ?-> A -> B): A -> Context ?-> B =
     a => f(a)
 
   def pushLeft[A, B](f: A -> Context ?-> B): Context ?-> A => B =
+
+    /**
+     * valid return types:
+     * (ct: Context) ?-> A => B^{ct} //redundant
+     * Context ?-> A => B^ //redundant
+     * Context ?-> A => B
+     * (using Context): A => B
+     * Context ?-> (A -> B)^
+     * (ct: Context) ?-> (A -> B)^{ct} //redundant
+     * (ct: Context) ?-> A ->{ct} B //redundant
+     * (using ct:Context): A ->{ct} B
+     * Context ?-> A -> Context ?-> B //verbose
+     *
+     * invalid:
+     * Context ?-> A -> B // Context^ is not allowed capture set
+     * (ct: Context) ?-> A -> B^{ct}
+     * `Context ?-> A -> B^`
+     */
     a => f(a)
-    //valid:
-    //  (ct: Context) ?-> A => B^{ct} //redundant
-    //  Context ?-> A => B^ //redundant
-    //  Context ?-> A => B
-    //  Context ?-> (A -> B)^
-    //  (ct: Context) ?-> (A -> B)^{ct}
-    //  Context ?-> A -> Context ?-> B //verbose
-    //
-    //invalid:
-    //  Context ?-> A -> B // Context^ is not allowed capture set
-    //  (ct: Context) ?-> A -> B^ct
-    //  Context ?-> A -> B^
 
 object WithMonad:
   def pushRight[A, B](f: Option[A -> B]): A -> Option[B] =
@@ -27,3 +35,19 @@ object WithMonad:
 
   def pushLeft[A, B](f: A -> Option[A]): Option[A -> B] =
     ??? // not possible to implement
+
+
+class Context @publicInBinary private extends caps.Capability:
+  inline def none: Nothing = throw Context.ControlNone
+
+object Context:
+  inline def none(using context: Context): Nothing = context.none
+
+  inline def run[A](inline body: Context ?=> A): Option[A] =
+    try
+      Option(body(using Context()))
+    catch
+      case ControlNone => None
+
+  private object ControlNone extends ControlThrowable("None") with NoStackTrace:
+    given CanEqual[ControlNone.type, Throwable] = CanEqual.derived
